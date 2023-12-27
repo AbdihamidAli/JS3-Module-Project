fe60281ba358f71e20290c326b84c65e94c2b050
AbdihamidAli <abdi747573@gmail.com>
2023-12-27 13:27:49 +0000

    level 100 first attempt

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+    const episodeElem = document.createElement("div");
+    episodeElem.classList.add("episode-container");
+
+    const episodeDisplay = formatEpisodeDisplay(episode.season, episode.number);
+    episodeElem.innerHTML = `
+      <div class="episode-info" id="content">${episode.name} - ${episodeDisplay}</div>
+      <img src="${episode.image.medium}" alt="${episode.name}" />
+      <p>${episode.summary}</p>
+    `;
+
+    rootElem.appendChild(episodeElem);
+  });
+}
+
+function formatEpisodeDisplay(season, number) {
+  return `S${String(season).padStart(2, "0")}E${String(number).padStart(
+    2,
+    "0"
+  )}`;
 }
 
 window.onload = setup;
